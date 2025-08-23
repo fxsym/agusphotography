ccce16c54e079200e8bd19f2eb4b123ab6ed689b
M Afrian Maulana <afrrianmaulana965@gmail.com>
2025-08-23 21:24:04 +0700

    Menambah paragraf baru

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,6 +2,7 @@ export default function Description() {
     return(
         <div>
             <p className="text-3xl">Description</p>
+            <p className="text-3xl">Halo nama saya agus</p>
         </div>
     )
 }
