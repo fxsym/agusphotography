38e23dec544a06758d23d4422e7a63449f885af3
M Afrian Maulana <afrrianmaulana965@gmail.com>
2025-09-20 22:01:54 +0700

    pesan commit kamu

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,37 +6,37 @@ export default function Contact() {
       <div className="max-w-3xl mx-auto text-center">
         <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
         <p className="text-gray-600 mb-10">
-          Jika kamu ingin bekerja sama atau sekadar menyapa, hubungi saya melalui kontak berikut:
+          If you want to collaborate or just say hello, contact me via the following contact details:
         </p>
 
         <div className="grid gap-6 md:grid-cols-3">
           {/* Nomor Telepon */}
           <a
-            href="tel:+628123456789"
+            href="tel:+81 80-5834-4559"
             className="flex flex-col items-center bg-white shadow-md rounded-2xl p-6 hover:shadow-lg hover:scale-105 transition"
           >
             <Phone size={32} className="text-blue-500 mb-3" />
-            <span className="font-semibold">+62 812-3456-789</span>
+            <span className="font-semibold">+81 80-5834-4559</span>
           </a>
 
           {/* Email */}
           <a
-            href="mailto:emailkamu@example.com"
+            href="mailto:aguswidodo1197@gmail.coms"
             className="flex flex-col items-center bg-white shadow-md rounded-2xl p-6 hover:shadow-lg hover:scale-105 transition"
           >
             <Mail size={32} className="text-red-500 mb-3" />
-            <span className="font-semibold">emailkamu@example.com</span>
+            <span className="font-semibold">aguswidodo1197@gmail.com</span>
           </a>
 
           {/* Instagram */}
           <a
-            href="https://instagram.com/usernamekamu"
+            href="https://www.instagram.com/agus_phto?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
             target="_blank"
             rel="noopener noreferrer"
             className="flex flex-col items-center bg-white shadow-md rounded-2xl p-6 hover:shadow-lg hover:scale-105 transition"
           >
             <Instagram size={32} className="text-pink-500 mb-3" />
-            <span className="font-semibold">@usernamekamu</span>
+            <span className="font-semibold">@agus_photo</span>
           </a>
         </div>
       </div>
